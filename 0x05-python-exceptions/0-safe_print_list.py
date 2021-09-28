9a010a2bef623ba063ba02a352ac1e0d0587b5d2
Hanifa-10 <chep.hanifa@fmail.com>
2021-09-28 07:55:12 -0700

    updating files

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter = 0
-    try:
-        for item in my_list[:x]:
-            print("{}".format(item), end="")
-            counter += 1
-            print()
-    except:
-        print("An error occured, try again")
-        return counter
+    i = 0
+    while i < x:
+        try:
+            print(my_list[i], end="")
+            i += 1
+        except IndexError:
+            break
+        print('')
+        return (i)
+
 
