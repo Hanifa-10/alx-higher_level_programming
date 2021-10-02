82615e5b71acd0a214b0fe6a853b324e48c4d412
Hanifa-10 <chep.hanifa@fmail.com>
2021-10-02 04:38:30 -0700

    add integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is 0-add_integer module supplied with one function, add_integer(a, b)
+"""
+
+def add_integer(a, b):
+    """Return the sum of two integer numbers"""
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
