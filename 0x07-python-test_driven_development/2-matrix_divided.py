c52bd4ec2d151657350ee313f77d4f55c19f210b
Hanifa-10 <84616522+Hanifa-10@users.noreply.github.com>
2021-10-03 15:22:46 +0300

    Update 2-matrix_divided.py

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+"2-matrix_divided" module containing only one function provided:"Matrix_divided"
+"""
+
+
+def matrix_divided(matrix, div):
+    """Return a list that contain all elements of matrix divided"""
+
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ValueError("division by zero")
+    for elm in matrix:
+        if type(elm) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if len(elm) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in elm:
+            if type(i) is not int and float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    return([list(map(lambda x: round(x/div, 2), row)) for row in matrix])
